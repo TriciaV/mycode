e0d39a4366be619a0dc184f141e35618c1bbc33a
TriciaV <tverbanic@yahoo.com>
2023-01-26 20:01:39 +0000

    crontab

---
+++ cron-work/cron1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+##  CRON
+#Hop over to https://crontab.guru/ and experiment with cron timing. 
+#It's done with a series of 5 asterisks (*), where each asterisk represents a different value, and an asterisk always a wildcard.
+
+import time
+print("\nGMO-Free Grass Fed Human Time - " + time.ctime())
+
